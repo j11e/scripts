a94a883f245a58437b771e34b86e1ab010cbc8fc
JD Innocenti <ajin@alphanetworks.be>
2017-12-13 17:30:37 +0100

    idea for a php_docker.sh v2

--- php_docker.sh
+++ php_docker.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# exercise to the reader: add support for composer intall/update
+# maybe a simple "if $2 == composer" (with a tad more cleverness in case there a path to follow, etc)
+# that mounts some additional folders and composes the corresponding docker command
+
 dockerphp() {
   filepath=$(dirname $(realpath $1))
   filename=$(basename $(realpath $1))
